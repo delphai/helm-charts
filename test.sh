192904abfbe39d9d62323b843269da21842a81f2
ahmed mahmoud <ahmed-delphai@ahmeds-Air.fritz.box>
2020-12-04 16:20:44 +0100

    add models. in machine-learning

--- test.sh
+++ test.sh
@@ -1,22 +1,22 @@
 #! /usr/bin/env bash
 set -e
-kubectx delphai-staging
+kubectx delphai-common
 
-RELEASE_NAME=query-resolution
+RELEASE_NAME=page-classification-bentoml
 
-REPO_NAME=query-resolution
+REPO_NAME=page-classification-bentoml
 
-IMAGE=delphaistaging.azurecr.io/query-resolution@sha256:da99cb425632a4f7eca5b1f3dcd8e131f165aa476d76e2358749b57184ac6555
+IMAGE=delphaicommon.azurecr.io/page-classification-bentoml@sha256:94cf3fce78f5dc465227c7f61a5332042c24b0289eea2f5d45f0e4b0e848509b
 
 kubectl create namespace ${REPO_NAME} --output yaml --dry-run=client | kubectl apply -f -
 kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
 DOMAIN=$(kubectl get secret domain -o json --namespace default | jq .data.domain -r | base64 -d)
-DOMAINS=""
 helm upgrade --install --atomic  --reset-values\
     ${RELEASE_NAME} \
-    ./charts/delphai-knative-service \
+    ./charts/delphai-machine-learning \
     --namespace=${REPO_NAME} \
     --set domain=${DOMAIN} \
     --set image=${IMAGE} \
+    --set httpPort=5000 \
     --set domains="" \
-    --set delphaiEnvironment=staging
+    --set delphaiEnvironment=common
