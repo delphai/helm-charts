12b97ed009e67ab3b8e3da14176dadb3f3d1d1c6
ahmed <s0565961@htw-berlin.de>
2021-01-18 16:31:47 +0100

    fix: remove grpc services from ML chart

--- test.sh
+++ test.sh
@@ -1,22 +1,23 @@
 #! /usr/bin/env bash
 set -e
-kubectx delphai-staging
+kubectx delphai-common
 
+RELEASE_NAME=page-classification-bentoml
 
-RELEASE_NAME=subdomain-test
+REPO_NAME=page-classification-bentoml
 
-REPO_NAME=subdomain-test
-
-IMAGE=delphaistaging.azurecr.io/delphai-ui-v2@sha256:10f66df4b3a5724e438431c995109e02a1ca7b7e404e6155ba0d1c872014b361
+IMAGE=delphaicommon.azurecr.io/page-classification-bentoml@sha256:94cf3fce78f5dc465227c7f61a5332042c24b0289eea2f5d45f0e4b0e848509b
 
 kubectl create namespace ${REPO_NAME} --output yaml --dry-run=client | kubectl apply -f -
 kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
 DOMAIN=$(kubectl get secret domain -o json --namespace default | jq .data.domain -r | base64 -d)
 helm upgrade --install --atomic  --reset-values\
     ${RELEASE_NAME} \
-    ./charts/delphai-with-ui \
+    ./charts/delphai-machine-learning \
     --namespace=${REPO_NAME} \
     --set domain=${DOMAIN} \
     --set image=${IMAGE} \
-    --set httpPort=80 \
-    --set delphaiEnvironment=development 
+    --set httpPort=5000 \
+    --set delphaiEnvironment=common \
+    --set minScale=1 \
+    --set concurrency=50
