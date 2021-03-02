b896c94b23b4b2ddc9469809be31462b54aecda0
ahmed <s0565961@htw-berlin.de>
2021-03-02 17:00:09 +0100

    feat: istio sidecar retry 10 times

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 set -e
-kubectx delphai-common
+kubectx delphai-development
 
-RELEASE_NAME=page-classification-bentoml
+RELEASE_NAME=busybox
 
-REPO_NAME=page-classification-bentoml
+REPO_NAME=busybox
 
 IMAGE=delphaicommon.azurecr.io/page-classification-bentoml@sha256:94cf3fce78f5dc465227c7f61a5332042c24b0289eea2f5d45f0e4b0e848509b
 
@@ -13,11 +13,5 @@ kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSe
 DOMAIN=$(kubectl get secret domain -o json --namespace default | jq .data.domain -r | base64 -d)
 helm upgrade --install --atomic  --reset-values\
     ${RELEASE_NAME} \
-    ./charts/delphai-machine-learning \
-    --namespace=${REPO_NAME} \
-    --set domain=${DOMAIN} \
-    --set image=${IMAGE} \
-    --set httpPort=5000 \
-    --set delphaiEnvironment=common \
-    --set minScale=1 \
-    --set concurrency=50
+    ./charts/delphai-knative-service \
+    --namespace=${REPO_NAME}
