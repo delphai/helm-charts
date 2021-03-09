91cca3014f7e3240468395b0c97d08851d218337
ahmed <s0565961@htw-berlin.de>
2021-03-09 14:28:10 +0100

    feat: add job to machine-learning

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
 #! /usr/bin/env bash
 set -e
-kubectx delphai-development
+kubectx delphai-hybrid
 
-RELEASE_NAME=busybox
+RELEASE_NAME=train-model-gpu
 
-REPO_NAME=busybox
+REPO_NAME=train-model-gpu
 
-IMAGE=delphaicommon.azurecr.io/page-classification-bentoml@sha256:94cf3fce78f5dc465227c7f61a5332042c24b0289eea2f5d45f0e4b0e848509b
+IMAGE=delphaicommon.azurecr.io/train-model-gpu:latest
 
 kubectl create namespace ${REPO_NAME} --output yaml --dry-run=client | kubectl apply -f -
 kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
-DOMAIN=$(kubectl get secret domain -o json --namespace default | jq .data.domain -r | base64 -d)
 helm upgrade --install --atomic  --reset-values\
     ${RELEASE_NAME} \
-    ./charts/delphai-knative-service \
-    --namespace=${REPO_NAME}
+    ./charts/delphai-machine-learning \
+    --namespace=${REPO_NAME} \
+    --set domain='delphai.site' \
+    --set image=${IMAGE} \
+    --set train=true \
+    --set delphaiEnvironment=ml \
+    --set gpu=true
+
+
