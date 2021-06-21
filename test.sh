7403d31b0cfa0415deea7d59e71eaa837fc7b0d2
ahmed <ahmed.mahmoud@delphai.com>
2021-06-21 21:12:00 +0200

    fix: ksvc url in virsual service http

--- test.sh
+++ test.sh
@@ -2,22 +2,32 @@
 set -e
 kubectx delphai-hybrid
 
-RELEASE_NAME=ml-utils
+REPO_NAME=news-event-classification-bentoml
 
-REPO_NAME=ml-utils
 
-IMAGE=delphaicommon.azurecr.io/ml-utils:latest
+IMAGE="delphai.azurecr.io/news-event-classification-bentoml@sha256:4964e076649c1b22b78ab34e018b92c85087942bdaa3249420f63cd701a21aca"
 
 kubectl create namespace ${REPO_NAME} --output yaml --dry-run=client | kubectl apply -f -
 kubectl patch serviceaccount default --namespace ${REPO_NAME} -p "{\"imagePullSecrets\": [{\"name\": \"acr-credentials\"}]}"
-helm upgrade --install --atomic  --reset-values\
-    ${RELEASE_NAME} \
-    ./charts/delphai-machine-learning \
-    --namespace=${REPO_NAME} \
-    --set domain='delphai.site' \
-    --set image=${IMAGE} \
-    --set train=true \
-    --set delphaiEnvironment=ml \
-    --set gpu=true
+helm upgrade --install \
+            --wait \
+            --force \
+            --namespace ${REPO_NAME} \
+            ${REPO_NAME} \
+            ./charts/delphai-machine-learning \
+            --set image=${IMAGE} \
+            --set domain=delphai.red \
+            --set delphaiEnvironment=hybrid\
+            --set httpPort=5000 \
+            --set minScale=1 \
+            --set concurrency=30
 
-
+# helm template --namespace ${REPO_NAME} \
+#             ${REPO_NAME} \
+#             ./charts/delphai-machine-learning \
+#             --set image=${REPO_NAME} \
+#             --set domain=delphai.red \
+#             --set delphaiEnvironment=hybrid\
+#             --set httpPort=5000 \
+#             --set minScale=1 \
+#             --set concurrency=30 > x.yml
